37fbde419ae17a490350f5b02a17dd5770920264
Leonardo Papst <leopapst@hotmail.com>
2025-11-19 21:48:20 -0300

    Merge master to main

---
+++ Jokenpo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Jokenpo;
+using Jokenpo.Application.Services;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var regra = new JogoRegrasService();
+        var jogo = new JogoService(regra);
+
+        var ui = new ConsoleUI(jogo);
+        ui.Iniciar();
+    }
+}
